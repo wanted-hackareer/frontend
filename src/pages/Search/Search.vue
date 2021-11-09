9cf242971fe8cc4e7f26c8fabf88f5b824ce5f26
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:30:23 +0900

    [add] 검색 페이지 추가

---
+++ src/pages/Search/Search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="검색" :hasCartTab="true" />
+        <div class="app-container">
+            <SearchInputComponent />
+            <div class="search-filterd-content">검색 페이지입니다.</div>
+            <div>
+                <div class="search-add-cart__button">카트에 담기</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import SearchInputComponent from "../../components/SearchInputComponent/SearchInputComponent.vue";
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "Search",
+    components: {
+        SearchInputComponent,
+        Header,
+    },
+};
+</script>
+<style scoped>
+@import "./Search.css";
+</style>
