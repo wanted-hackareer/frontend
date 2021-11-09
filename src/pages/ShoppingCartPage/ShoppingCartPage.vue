141b04322b6446f5f07401add69c534080d8bdf7
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:28:33 +0900

    [add] 장바구니 페이지 추가

---
+++ src/pages/ShoppingCartPage/ShoppingCartPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="나의 장바구니" />
+        <div class="app-container">
+            <div>장바구니 페이지입니다.</div>
+        </div>
+    </div>
+</template>
+<script>
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "ShoppingCartPage",
+    components: {
+        Header,
+    },
+};
+</script>
+<style scoped>
+@import "./ShoppingCartPage.css";
+</style>
