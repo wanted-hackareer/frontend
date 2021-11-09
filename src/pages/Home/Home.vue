8e7a9fc841385e681467ca98f58886fadbde588c
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:30:39 +0900

    [add] 메인 페이지 추가

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Header :hasCartTab="true" headerNavigationTitle="다믐" />
+        <div class="app-container">
+            <div>
+                <div class="home-introduce-content">
+                    <div class="profile-image"></div>
+                    <div class="profile-introduce-text">오늘은 어떤 장을 <br />함께 볼까요?</div>
+                </div>
+                <SearchInputComponent />
+            </div>
+            <div class="home-cart-empty">장바구니가 비었습니다.</div>
+        </div>
+    </div>
+</template>
+<script>
+import SearchInputComponent from "../../components/SearchInputComponent/SearchInputComponent.vue";
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "Home",
+    components: {
+        Header,
+        SearchInputComponent,
+    },
+};
+</script>
+<style scoped>
+@import "./Home.css";
+</style>
