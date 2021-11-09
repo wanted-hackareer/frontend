ed551a0c91c739d48f31f91e145ae74a64f803c3
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:28:46 +0900

    [add] 모임 페이지 추가

---
+++ src/pages/MeetingPage/MeetingPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="모임" />
+        <div class="app-container">
+            <div>모임 페이지입니다.</div>
+        </div>
+    </div>
+</template>
+<script>
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "MeetingPage",
+    components: {
+        Header,
+    },
+};
+</script>
+<style scoped>
+@import "./MeetingPage.css";
+</style>
