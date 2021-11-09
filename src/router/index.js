60c4d03dee24523966b7edaab1f63086c324c8cc
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:30:55 +0900

    [update] router path 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../pages/Home/Home.vue";
+import Search from "../pages/Search/Search.vue";
+import ShoppingCartPage from "../pages/ShoppingCartPage/ShoppingCartPage.vue";
+import MeetingPage from "../pages/MeetingPage/MeetingPage.vue";
+import ChatRoomPage from "../pages/ChatRoomPage/ChatRoomPage.vue";
 Vue.use(VueRouter); // 라우트 정보
 
 const route = [
-    { path: "/", name: "Home", component: Home }, // 루트(/)로 접속했을때, Home 컴포넌트를 보여준다.
+    { path: "/", name: "Home", component: Home },
+    { path: "/search/:word?", name: "Search", component: Search },
+    { path: "/shoppingcart", name: "ShoppingCartPage", component: ShoppingCartPage },
+    { path: "/meeting", name: "MeetingPage", component: MeetingPage },
+    { path: "/chatroom", name: "ChatRoomPage", component: ChatRoomPage },
 ]; // Vue 라우터 인스턴스 생성
 
 const router = new VueRouter({ mode: "history", routes: route });
