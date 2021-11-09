ab6ee72000cbaa38536316b6a93592618c1ad47a
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 17:53:53 +0900

    [add] 푸터 컴포넌트 추가

---
+++ src/layout/Footer/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <!-- user bottom navigation -->
+        <aside class="bottom-nav footer--view">
+            <router-link
+                v-for="footerItem in footerList"
+                :key="footerItem.name"
+                :to="footerItem.path"
+                class="footer-btn"
+                :class="{ 'footer-btn--active': $route.path === footerItem.path }"
+            >
+                <div>{{ footerItem.name }}</div>
+            </router-link>
+        </aside>
+    </div>
+</template>
+<script>
+export default {
+    name: "Footer",
+    data() {
+        return {
+            footerList: [
+                {
+                    name: "홈",
+                    path: "/",
+                },
+                {
+                    name: "채팅방",
+                    path: "/",
+                },
+                {
+                    name: "모임",
+                    path: "/",
+                },
+            ],
+        };
+    },
+};
+</script>
+<style scoped>
+@import "./Footer.css";
+</style>
