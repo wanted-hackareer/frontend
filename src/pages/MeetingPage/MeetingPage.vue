0591b0d7ce09c0e3e5e84f4eccae99dcfaf17cf6
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 01:34:50 +0900

    [add] 모임 리스트 페이지 추가

--- src/pages/MeetingPage/MeetingPage.vue
+++ src/pages/MeetingPage/MeetingPage.vue
@@ -2,17 +2,30 @@
     <div>
         <Header headerNavigationTitle="모임" />
         <div class="app-container">
-            <div>모임 페이지입니다.</div>
+            <div class="meeting-container">
+                <div class="meeting-description">3900개의 모임이 기다리고 있어요</div>
+                <div>
+                    <MeetingChatComponent />
+                    <MeetingChatComponent />
+                    <MeetingChatComponent />
+                    <MeetingChatComponent />
+                    <MeetingChatComponent />
+                </div>
+            </div>
+        </div>
+        <div>
+            <button class="meeting-write__button"></button>
         </div>
     </div>
 </template>
 <script>
 import Header from "../../layout/Header/Header.vue";
-
+import MeetingChatComponent from "../../components/MeetingChatComponent/MeetingChatComponent.vue";
 export default {
     name: "MeetingPage",
     components: {
         Header,
+        MeetingChatComponent,
     },
 };
 </script>
