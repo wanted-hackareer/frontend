e5b3b43b1d4885d5b8a6a0db680b364ce3fd570d
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-09 21:29:41 +0900

    [add] 검색 버튼 컴포넌트 추가

---
+++ src/components/SearchInputComponent/SearchInputComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="search-wrap">
+        <input type="texxt" class="select__input" placeholder="구매하고 싶은 항목을 검색해보세요." />
+        <router-link to="/search">
+            <button><img src="" alt="search icon" /></button>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: "SearchInputComponent",
+};
+</script>
+<style scoped>
+@import "./SearchInputComponent.css";
+</style>
