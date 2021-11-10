4040fa12ff76d48cfd1283b6e6687f1f087fa60b
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:22:13 +0900

    [add] 채팅방 페이지 추가

--- src/pages/ChatRoomPage/ChatRoomPage.vue
+++ src/pages/ChatRoomPage/ChatRoomPage.vue
@@ -1,18 +1,35 @@
 <template>
     <div>
-        <Header headerNavigationTitle="채팅방" />
+        <Header :hasAlertTab="true" headerNavigationTitle="채팅방" />
         <div class="app-container">
-            <div>채팅방 페이지입니다.</div>
+            <div class="chat-room-container">
+                <SearchInputComponent />
+                <div class="chat-room-my-meeting">
+                    <div class="description">나의 장보기 모임</div>
+                    <MyMeetingListComponent />
+                </div>
+                <div class="chat-room-my-chat">
+                    <div class="description">채팅</div>
+                    <ChattingListComponent />
+                    <ChattingListComponent />
+                    <ChattingListComponent />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
 import Header from "../../layout/Header/Header.vue";
-
+import SearchInputComponent from "../../components/SearchInputComponent/SearchInputComponent.vue";
+import ChattingListComponent from "../../components/ChattingListComponent/ChattingListComponent.vue";
+import MyMeetingListComponent from "../../components/MyMeetingListComponent/MyMeetingListComponent.vue";
 export default {
     name: "ChatRoomPage",
     components: {
         Header,
+        SearchInputComponent,
+        ChattingListComponent,
+        MyMeetingListComponent,
     },
 };
 </script>
