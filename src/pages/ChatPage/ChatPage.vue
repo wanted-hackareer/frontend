e9053e628520f1cc917e720ab14765a720006739
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:23:31 +0900

    [add] 채팅방 페이지 추가

---
+++ src/pages/ChatPage/ChatPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="채팅" />
+        <div class="app-container">
+            <div>채팅방 페이지입니다.</div>
+        </div>
+    </div>
+</template>
+<script>
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "ChatPage",
+    components: {
+        Header,
+    },
+};
+</script>
+<style scoped>
+@import "./ChatPage.css";
+</style>
