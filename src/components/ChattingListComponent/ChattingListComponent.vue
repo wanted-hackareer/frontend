06b24e06c03c2710c3eb6ff1f40a2d75823ff4bf
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:21:57 +0900

    [add] 채팅방 목록 컴포넌트 추가

---
+++ src/components/ChattingListComponent/ChattingListComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="chat-list-container">
+        <div class="chat-item-header">
+            <div class="chat-title">아침 재료 함께 사요 모임</div>
+            <div class="chat-date">11월 6일</div>
+        </div>
+        <div class="chat-participants">두더지(나) 1</div>
+        <div class="chat-description">함께 할 팀원을 기다리고 있어요</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "ChattingListComponent",
+    components: {},
+};
+</script>
+<style scoped>
+@import "./ChattingListComponent.css";
+</style>
