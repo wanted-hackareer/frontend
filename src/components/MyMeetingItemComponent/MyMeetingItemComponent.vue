f32b099dea9a46baa5f8c502442df4523ab419f0
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:22:27 +0900

    [add] 나의 모임 컴포넌트 추가

---
+++ src/components/MyMeetingItemComponent/MyMeetingItemComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="meeting-item-container">
+        <div class="meeting-item-location">동남구 안서동</div>
+        <div class="meeting-item-title">아침 재료 함께 사요 
+			111111111111111111어디까지 써지나 123456789</div>
+        <div class="meeting-item-tag-list">
+            <div class="meeting-item-tag">D-DAY</div>
+            <div class="meeting-item-tag">1명</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyMeetingItemComponent",
+    components: {},
+};
+</script>
+<style scoped>
+@import "./MyMeetingItemComponent.css";
+</style>
