b7cdfc344e035a1dc221c4180581bdb628b451d7
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:22:48 +0900

    [add] 나의 채팅방 목록 컴포넌트 추가

---
+++ src/components/MyMeetingListComponent/MyMeetingListComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="meeting-list-container">
+        <MyMeetingItemComponent />
+        <button class="meeting-add-button" @click="goToWritePage">
+            <div>+</div>
+            <div class="meeting-add-button-description">추가하기</div>
+        </button>
+    </div>
+</template>
+<script>
+import MyMeetingItemComponent from "../MyMeetingItemComponent/MyMeetingItemComponent.vue";
+export default {
+    name: "MyMeetingListComponent",
+    components: { MyMeetingItemComponent },
+    methods: {
+        goToWritePage() {
+            this.$router.push({ path: "/write" });
+        },
+    },
+};
+</script>
+<style scoped>
+@import "./MyMeetingListComponent.css";
+</style>
