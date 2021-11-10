ad9491a36ae9d596314ce565da7ad7d3abf6f9c0
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:23:53 +0900

    [add] 라우터 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,25 @@ import VueRouter from "vue-router";
 import Home from "../pages/Home/Home.vue";
 import Search from "../pages/Search/Search.vue";
 import ShoppingCartPage from "../pages/ShoppingCartPage/ShoppingCartPage.vue";
+import LocationPage from "../pages/LocationPage/LocationPage.vue";
 import MeetingPage from "../pages/MeetingPage/MeetingPage.vue";
 import ChatRoomPage from "../pages/ChatRoomPage/ChatRoomPage.vue";
+import WriteMeetingPage from "../pages/WriteMeetingPage/WriteMeetingPage.vue";
+import AlertPage from "../pages/AlertPage/AlertPage.vue";
+import ChatPage from "../pages/ChatPage/ChatPage.vue";
+
 Vue.use(VueRouter); // 라우트 정보
 
 const route = [
     { path: "/", name: "Home", component: Home },
     { path: "/search/:word?", name: "Search", component: Search },
     { path: "/shoppingcart", name: "ShoppingCartPage", component: ShoppingCartPage },
+    { path: "/location", name: "LocationPage", component: LocationPage },
     { path: "/meeting", name: "MeetingPage", component: MeetingPage },
     { path: "/chatroom", name: "ChatRoomPage", component: ChatRoomPage },
+    { path: "/write", name: "WriteMeetingPage", component: WriteMeetingPage },
+    { path: "/alert", name: "AlertPage", component: AlertPage },
+    { path: "/chat/:id", name: "ChatPage", component: ChatPage },
 ]; // Vue 라우터 인스턴스 생성
 
 const router = new VueRouter({ mode: "history", routes: route });
