428ee6e8a5c2ca1b1611de5ce12c7285bd2d62a2
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 17:12:59 +0900

    [add] 회원가입 페이지 추가

---
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="회원가입" />
+        <div class="app-container">
+            <div class="register-container">
+                <div class="register-profile__image">사진</div>
+                <input class="register-nickname__input" />
+                <div class="register-input-container">
+                    <div class="register-email__input-text">아이디</div>
+                    <input class="register-input" type="text" />
+                    <div class="register-passwd__input-text">비밀번호</div>
+                    <input class="passwd-input" type="text" />
+                </div>
+                <div class="register-address-container">
+                    <div class="register-adderss-title">주소(필수)</div>
+                </div>
+            </div>
+            <router-link to="/login">
+                <div class="register__button">확인</div>
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+import Header from "../../layout/Header/Header.vue";
+
+export default {
+    name: "RegisterPage",
+    components: {
+        Header,
+    },
+};
+</script>
+<style scoped>
+@import "./RegisterPage.css";
+</style>
