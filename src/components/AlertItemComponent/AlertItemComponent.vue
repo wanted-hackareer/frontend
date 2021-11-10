9a0eb295734ab8b5e4e256c7d6ada4fdc9579791
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:21:31 +0900

    [add] 알람 목록 컴포넌트 추가

---
+++ src/components/AlertItemComponent/AlertItemComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="alert-item-container">
+        <div class="alert-item-header">
+            <div class="alert-item-time">1시간전</div>
+            <div class="alert-item-read">읽지 않음</div>
+        </div>
+        <div class="alert-item-main">다람쥐님이 아침거리 장보기 <br />모임 참여를 원합니다.</div>
+        <div class="alert-item-confirm-content">
+            <button class="alert-reject__button">거절</button>
+            <button class="alert-confirm__button" @click="goToChatRoom">수락</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "AlertItemComponent",
+    components: {},
+    methods: {
+        goToChatRoom() {
+            this.$router.push({ path: "/chat/1" });
+        },
+    },
+};
+</script>
+<style scoped>
+@import "./AlertItemComponent.css";
+</style>
---
+++ src/components/AlertListComponent/AlertListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="alert-list-container">
+            <AlertItemComponent />
+            <AlertItemComponent />
+        </div>
+    </div>
+</template>
+<script>
+import AlertItemComponent from "../AlertItemComponent/AlertItemComponent.vue";
+export default {
+    name: "AlertListComponent",
+    components: { AlertItemComponent },
+};
+</script>
+<style scoped>
+@import "./AlertListComponent.css";
+</style>
