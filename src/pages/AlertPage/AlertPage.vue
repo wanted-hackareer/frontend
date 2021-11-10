541479c59a7677a660f63892442b84777dbe2d3c
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-10 15:24:41 +0900

    [add] 알림 페이지 추가

---
+++ src/pages/AlertPage/AlertPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header headerNavigationTitle="알림" />
+        <div class="app-container">
+            <AlertListComponent />
+        </div>
+    </div>
+</template>
+<script>
+import Header from "../../layout/Header/Header.vue";
+import AlertListComponent from "../../components/AlertListComponent/AlertListComponent.vue";
+export default {
+    name: "AlertPage",
+    components: {
+        Header,
+        AlertListComponent,
+    },
+};
+</script>
+<style scoped>
+@import "./AlertPage.css";
+</style>
