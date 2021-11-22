c5b2b1030440ac47115acac107feb904cf035538
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-22 23:09:57 +0900

    [update] 홈페이지 수정

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -7,7 +7,7 @@
                     <div class="profile-image"></div>
                     <div class="profile-introduce-text">오늘은 어떤 장을 <br />함께 볼까요?</div>
                 </div>
-                <SearchInputComponent />
+                <SearchInputComponent :isCart="true" />
             </div>
             <div class="home-cart-empty">장바구니가 비었습니다.</div>
         </div>
