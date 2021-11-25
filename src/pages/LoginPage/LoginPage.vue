4118dbf0bc87e973b1d7eeb93671355dd3440b33
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-25 21:05:20 +0900

    [update] 로그인/회원가입 기능 추가

--- src/pages/LoginPage/LoginPage.vue
+++ src/pages/LoginPage/LoginPage.vue
@@ -3,24 +3,34 @@
         <Header :hasBackButton="false" headerNavigationTitle="로그인" />
         <div class="app-container">
             <div class="login-container">
-                <div class="app-logo"></div>
+                <div class="app-logo">
+                    <img src="../../assets/images/LoginPage/logo.png" />
+                </div>
                 <div class="login-text">로그인 해주세요.</div>
                 <div class="login-input-container">
-                    <div class="login-email__input-text">아이디</div>
-                    <input class="login-input" type="text" />
+                    <div class="login-email__input-text">이메일</div>
+                    <input class="login-input" v-model="userEmailValue" ref="userEmail" type="text" />
+                    <div v-if="errorList.emailBlank.length !== 0" class="login-email-error login-error">{{ errorList.emailBlank }}</div>
                     <div class="login-passwd__input-text">비밀번호</div>
-                    <input class="passwd-input" type="password" />
+                    <input class="passwd-input" v-model="userPasswordValue" ref="password" type="password" />
+                    <div v-if="errorList.passwordBlank.length !== 0" class="login-error">{{ errorList.passwordBlank }}</div>
+                    <div v-if="errorList.notFound.length !== 0" class="login-error">{{ errorList.notFound }}</div>
+                    <div v-else-if="errorList.wrong.length !== 0" class="login-error">{{ errorList.wrong }}</div>
                 </div>
                 <div class="login-footer">
                     <div class="login-save__email-content">
-                        <input class="login-save__email-checkbox" type="checkbox" />
-                        <div class="login-save__email">이메일 저장</div>
+                        <input
+                            id="emailSave"
+                            class="login-save__email-checkbox"
+                            type="checkbox"
+                            v-model="saveEmailCheckBoxStatusValue"
+                            :checked="saveEmailCheckBoxStatusValue"
+                        />
+                        <label for="emailSave" class="login-save__email">이메일 저장</label>
                     </div>
                     <div class="login-find__passwd">비밀번호 찾기</div>
                 </div>
-                <router-link to="/">
-                    <div class="login__button">로그인</div>
-                </router-link>
+                <div class="login__button" @click="clickLogin">로그인</div>
                 <router-link to="/register">
                     <div class="register__button">회원가입 하기</div>
                 </router-link>
@@ -30,11 +40,83 @@
 </template>
 <script>
 import Header from "../../layout/Header/Header.vue";
+import { createNamespacedHelpers } from "vuex";
+const userHelper = createNamespacedHelpers("userStore");
 
 export default {
     name: "LoginPage",
     components: {
         Header,
+    },
+
+    computed: {
+        ...userHelper.mapState({
+            userEmail: (state) => state.userEmail,
+            password: (state) => state.password,
+            errorList: (state) => state.errorList,
+            saveEmailCheckBoxStatus: (state) => state.saveEmailCheckBoxStatus,
+        }),
+        ...userHelper.mapGetters(["getErrorList"]),
+
+        userEmailValue: {
+            get() {
+                return localStorage.getItem("userEmail") ? localStorage.getItem("userEmail") : this.userEmail;
+            },
+            set(value) {
+                this.UPDATE_USER_EMAIL(value);
+            },
+        },
+
+        userPasswordValue: {
+            get() {
+                return this.password;
+            },
+            set(value) {
+                this.UPDATE_PASSWORD(value);
+            },
+        },
+        saveEmailCheckBoxStatusValue: {
+            get() {
+                return this.saveEmailCheckBoxStatus;
+            },
+            set(value) {
+                this.UPDATE_SAVE_EMAIL_CHECK_BOX_STATUS(value);
+            },
+        },
+    },
+    methods: {
+        ...userHelper.mapMutations(["UPDATE_USER_EMAIL", "UPDATE_PASSWORD", "UPDATE_SAVE_EMAIL_CHECK_BOX_STATUS"]),
+        ...userHelper.mapActions(["postSignIn"]),
+
+        checkValidation() {},
+        reset() {
+            this.errorList.emailBlank = "";
+            this.errorList.passwordBlank = "";
+        },
+        // 이메일 정규표현식 체크
+
+        validEmail() {
+            const re = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+            return re.test(this.userEmail);
+        },
+        clickLogin() {
+            this.reset();
+            if (this.userEmail.length === 0) {
+                this.errorList.emailBlank = "이메일을 입력해주세요.";
+                this.$refs.userEmail.focus();
+                return;
+            } else if (!this.validEmail()) {
+                this.errorList.emailBlank = "이메일 형식을 맞춰주세요.";
+                this.$refs.userEmail.focus();
+            } else if (this.password.length === 0) {
+                this.errorList.passwordBlank = "비밀번호를 입력해주세요.";
+                this.$refs.password.focus();
+                return;
+            } else {
+                this.reset();
+                this.postSignIn();
+            }
+        },
     },
 };
 </script>
--- src/pages/RegisterPage/RegisterPage.vue
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -4,52 +4,113 @@
         <div class="app-container">
             <div class="register-container">
                 <div class="register-profile__image">사진</div>
-                <input class="register-nickname__input" placeholder="닉네임 입력" />
+                <input class="register-nickname__input" v-model="userNickNameValue" ref="nickName" placeholder="닉네임 입력" />
+                <div v-if="errorList.nickNameBlank.length !== 0" class="login-error">{{ errorList.nickNameBlank }}</div>
                 <div class="register-input-container">
-                    <input class="register-input" type="text" placeholder="이메일" />
-                    <input class="passwd-input" type="password" placeholder="비밀번호" />
+                    <input class="register-input" type="text" v-model="userEmailValue" ref="userEmail" placeholder="이메일" />
+                    <div v-if="errorList.emailBlank.length !== 0" class="login-email-error login-error">{{ errorList.emailBlank }}</div>
+                    <input class="passwd-input" type="password" v-model="userPasswordValue" ref="password" placeholder="비밀번호" />
+                    <div v-if="errorList.passwordBlank.length !== 0" class="login-error">{{ errorList.passwordBlank }}</div>
                 </div>
                 <div class="register-address-container">
                     <div class="register-adderss-title">주소(필수)</div>
                     <SearchInputComponent :isRegister="true" @getSearchData="execDaumPostcode" />
-                    <div>{{ addressList.sido }} {{ addressList.sigungu }} {{ addressList.bname }}</div>
+                    <div>{{ addressList.city }} {{ addressList.district }} {{ addressList.street }}</div>
+                    <div v-if="errorList.addressBlank.length !== 0" class="login-error">{{ errorList.addressBlank }}</div>
                 </div>
             </div>
-            <router-link to="/login">
-                <div class="register__button" :class="{ 'register__button--active': addressList.active }">확인</div>
-            </router-link>
+            <div class="register__button" :class="{ 'register__button--active': addressList.active }" @click="clickSignUp">확인</div>
         </div>
     </div>
 </template>
 <script>
 import SearchInputComponent from "../../components/SearchInputComponent/SearchInputComponent.vue";
 import Header from "../../layout/Header/Header.vue";
+import { createNamespacedHelpers } from "vuex";
+const userHelper = createNamespacedHelpers("userStore");
 
 export default {
     name: "RegisterPage",
     data() {
-        return {
-            addressList: { id: 0, sido: "", sigungu: "", bname: "", active: false },
-        };
+        return {};
     },
     components: {
         Header,
         SearchInputComponent,
     },
+    computed: {
+        ...userHelper.mapState({
+            userEmail: (state) => state.userEmail,
+            password: (state) => state.password,
+            addressList: (state) => state.addressList,
+            userNickName: (state) => state.userNickName,
+            errorList: (state) => state.errorList,
+        }),
+        ...userHelper.mapGetters(["validateNickName", "validateUserName"]),
+
+        userEmailValue: {
+            get() {
+                return this.userEmail;
+            },
+            set(value) {
+                this.UPDATE_USER_EMAIL(value);
+            },
+        },
+
+        userPasswordValue: {
+            get() {
+                return this.password;
+            },
+            set(value) {
+                this.UPDATE_PASSWORD(value);
+            },
+        },
+
+        userNickNameValue: {
+            get() {
+                return this.userNickName;
+            },
+            set(value) {
+                this.UPDATE_USER_NICKNAME(value);
+            },
+        },
+    },
     methods: {
+        ...userHelper.mapMutations(["UPDATE_USER_EMAIL", "UPDATE_PASSWORD", "UPDATE_USER_NICKNAME"]),
+        ...userHelper.mapActions(["postSignUp", "resetErrorList"]),
+
         execDaumPostcode() {
             new window.daum.Postcode({
                 oncomplete: (data) => {
-                    this.addressList.sido = data.sido;
-                    this.addressList.sigungu = data.sigungu;
-                    this.addressList.bname = data.bname;
+                    this.addressList.city = data.sido;
+                    this.addressList.district = data.sigungu;
+                    this.addressList.street = data.bname;
                     this.addressList.active = true;
                 },
             }).open(this.$refs.embed);
         },
-
-        //매칭하기
-        startMatch() {},
+        validate() {
+            if (this.validateNickName()) {
+                this.$refs.nickName.focus();
+                return true;
+            } else if (this.validateUserName()) {
+                this.$refs.userEmail.focus();
+                return true;
+            } else if (this.validatePassword()) {
+                this.$refs.password.focus();
+                return true;
+            } else if (this.validateAddress()) {
+                return true;
+            } else return false;
+        },
+        clickSignUp() {
+            this.resetErrorList();
+            if (this.validate()) return;
+            // else {
+            //     this.resetErrorList();
+            //     this.postSignUp();
+            // }
+        },
     },
 };
 </script>
