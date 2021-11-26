0c26a048b81318c43c1c3f906105aa998782939f
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-26 18:18:46 +0900

    [update] 회원가입 페이지 수정

--- src/pages/RegisterPage/RegisterPage.vue
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -3,20 +3,20 @@
         <Header headerNavigationTitle="회원가입" />
         <div class="app-container">
             <div class="register-container">
-                <div class="register-profile__image">사진</div>
-                <input class="register-nickname__input" v-model="userNickNameValue" ref="nickName" placeholder="닉네임 입력" />
-                <div v-if="errorList.nickNameBlank.length !== 0" class="login-error">{{ errorList.nickNameBlank }}</div>
+                <div class="register-profile__image"></div>
+                <input class="register-nickname__input" v-model="userNickName" ref="nickName" placeholder="닉네임 입력" />
+                <div v-if="errorList.nickNameBlank.length !== 0" class="error-text">{{ errorList.nickNameBlank }}</div>
                 <div class="register-input-container">
-                    <input class="register-input" type="text" v-model="userEmailValue" ref="userEmail" placeholder="이메일" />
-                    <div v-if="errorList.emailBlank.length !== 0" class="login-email-error login-error">{{ errorList.emailBlank }}</div>
-                    <input class="passwd-input" type="password" v-model="userPasswordValue" ref="password" placeholder="비밀번호" />
-                    <div v-if="errorList.passwordBlank.length !== 0" class="login-error">{{ errorList.passwordBlank }}</div>
+                    <input class="register-input" type="text" v-model="userEmail" ref="userEmail" placeholder="이메일" />
+                    <div v-if="errorList.emailBlank.length !== 0" class="login-email-error error-text">{{ errorList.emailBlank }}</div>
+                    <input class="passwd-input" type="password" v-model="userPassword" ref="password" placeholder="비밀번호" />
+                    <div v-if="errorList.passwordBlank.length !== 0" class="error-text">{{ errorList.passwordBlank }}</div>
                 </div>
                 <div class="register-address-container">
                     <div class="register-adderss-title">주소(필수)</div>
                     <SearchInputComponent :isRegister="true" @getSearchData="execDaumPostcode" />
                     <div>{{ addressList.city }} {{ addressList.district }} {{ addressList.street }}</div>
-                    <div v-if="errorList.addressBlank.length !== 0" class="login-error">{{ errorList.addressBlank }}</div>
+                    <div v-if="errorList.addressBlank.length !== 0" class="address-error error-text">{{ errorList.addressBlank }}</div>
                 </div>
             </div>
             <div class="register__button" :class="{ 'register__button--active': addressList.active }" @click="clickSignUp">확인</div>
@@ -31,53 +31,21 @@ const userHelper = createNamespacedHelpers("userStore");
 
 export default {
     name: "RegisterPage",
-    data() {
-        return {};
-    },
     components: {
         Header,
         SearchInputComponent,
     },
-    computed: {
-        ...userHelper.mapState({
-            userEmail: (state) => state.userEmail,
-            password: (state) => state.password,
-            addressList: (state) => state.addressList,
-            userNickName: (state) => state.userNickName,
-            errorList: (state) => state.errorList,
-        }),
-        ...userHelper.mapGetters(["validateNickName", "validateUserName"]),
-
-        userEmailValue: {
-            get() {
-                return this.userEmail;
-            },
-            set(value) {
-                this.UPDATE_USER_EMAIL(value);
-            },
-        },
-
-        userPasswordValue: {
-            get() {
-                return this.password;
-            },
-            set(value) {
-                this.UPDATE_PASSWORD(value);
-            },
-        },
-
-        userNickNameValue: {
-            get() {
-                return this.userNickName;
-            },
-            set(value) {
-                this.UPDATE_USER_NICKNAME(value);
-            },
-        },
+    data() {
+        return {
+            userNickName: "",
+            userEmail: "",
+            userPassword: "",
+            addressList: { city: "", district: "", street: "", active: false },
+            errorList: { nickNameBlank: "", emailBlank: "", passwordBlank: "", addressBlank: "" },
+        };
     },
     methods: {
-        ...userHelper.mapMutations(["UPDATE_USER_EMAIL", "UPDATE_PASSWORD", "UPDATE_USER_NICKNAME"]),
-        ...userHelper.mapActions(["postSignUp", "resetErrorList"]),
+        ...userHelper.mapActions(["postSignUp"]),
 
         execDaumPostcode() {
             new window.daum.Postcode({
@@ -89,27 +57,68 @@ export default {
                 },
             }).open(this.$refs.embed);
         },
-        validate() {
-            if (this.validateNickName()) {
+        resetErrorList() {
+            this.errorList.nickNameBlank = "";
+            this.errorList.emailBlank = "";
+            this.errorList.passwordBlank = "";
+            this.errorList.addressBlank = "";
+        },
+        validateNickName() {
+            if (this.userNickName.length === 0) {
+                this.errorList.nickNameBlank = "닉네임을 입력해주세요.";
                 this.$refs.nickName.focus();
                 return true;
-            } else if (this.validateUserName()) {
+            }
+        },
+
+        validateUserEmail() {
+            const re = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+            if (this.userEmail.length === 0) {
+                this.errorList.emailBlank = "이메일을 입력해주세요.";
                 this.$refs.userEmail.focus();
                 return true;
-            } else if (this.validatePassword()) {
-                this.$refs.password.focus();
-                return true;
-            } else if (this.validateAddress()) {
+            } else if (!re.test(this.userEmail)) {
+                this.errorList.emailBlank = "이메일 형식을 맞춰주세요.";
+                this.$refs.userEmail.focus();
                 return true;
             } else return false;
         },
-        clickSignUp() {
+
+        validatePassword() {
+            if (this.userPassword.length === 0) {
+                this.errorList.passwordBlank = "비밀번호를 입력해주세요.";
+                this.$refs.password.focus();
+                return true;
+            } else return false;
+        },
+        validateAddress() {
+            if (this.addressList["city"].length === 0) {
+                this.errorList.addressBlank = "주소를 선택해주세요.";
+                return true;
+            } else return false;
+        },
+
+        validate() {
             this.resetErrorList();
+            if (this.validateNickName()) {
+                return true;
+            } else if (this.validateUserEmail()) {
+                return true;
+            } else if (this.validatePassword()) {
+                return true;
+            } else if (this.validateAddress()) {
+                return true;
+            }
+            return false;
+        },
+        clickSignUp() {
             if (this.validate()) return;
-            // else {
-            //     this.resetErrorList();
-            //     this.postSignUp();
-            // }
+            this.postSignUp({
+                userEmail: this.userEmail,
+                userNickName: this.userNickName,
+                password: this.userPassword,
+                addressList: { city: this.addressList.city, district: this.addressList.district, street: this.addressList.street },
+            });
         },
     },
 };
