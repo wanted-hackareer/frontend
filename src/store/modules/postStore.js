d3257716b58f5c2fd2a538b19da1e1011f4bd123
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-26 18:20:13 +0900

    [update] 게시판 store 수정

--- src/store/modules/postStore.js
+++ src/store/modules/postStore.js
@@ -1,10 +1,17 @@
-// import axios from "axios";
-// const API_SERVER_URL = "http://localhost:3000";
+import axios from "axios";
+import router from "../../router";
+
+const API_SERVER_URL = "http://fa65-1-230-138-165.ngrok.io/api/v1";
 
 // 게시판 Store
 const postStore = {
     namespaced: true,
-    state: { postTitle: "", postDesc: "", postTag: "" },
+    state: {
+        postCount: 0,
+        postList: [],
+        postTitle: "",
+        postDesc: "",
+    },
     getters: {},
     mutations: {
         // v-model data 관리
@@ -15,12 +22,41 @@ const postStore = {
         UPDATE_POST_DESC(state, payload) {
             state.postDesc = payload;
         },
+    },
+    actions: {
+        // 게시물 등록
+        async postBoardData({ state, rootState, dispatch }, { dayOfTheWeek, tagList }) {
+            await axios
+                .post(
+                    `${API_SERVER_URL}/post`,
+                    { title: state.postTitle, description: state.postDesc, dayOfTheWeek, maximum: 3 },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${rootState.userStore.userToken}`,
+                        },
+                    }
+                )
+                .then(({ data }) => {
+                    const newTagList = tagList.map((i) => [{ ...i, postId: data.id }])[0];
+                    dispatch("postTagData", { tagList: newTagList });
+                })
+                .catch((error) => {
+                    console.log(error.response);
+                });
+        },
 
-        UPDATE_POST_TAG(state, payload) {
-            state.postTag = payload;
+        // 게시물 등록
+        async postTagData(_, { tagList }) {
+            await axios
+                .post(`${API_SERVER_URL}/tags`, tagList)
+                .then(() => {
+                    router.replace({ path: "/chatroom" });
+                })
+                .catch((error) => {
+                    console.log(error.response);
+                });
         },
     },
-    actions: {},
 };
 
 export default postStore;
