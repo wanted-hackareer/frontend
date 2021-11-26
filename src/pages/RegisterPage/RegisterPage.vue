b08524443083f15087aa33ef75ee8d7310d49e56
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-26 23:06:59 +0900

    [update] 회원가입 페이지 수정

--- src/pages/RegisterPage/RegisterPage.vue
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -5,20 +5,28 @@
             <div class="register-container">
                 <div class="register-profile__image"></div>
                 <input class="register-nickname__input" v-model="userNickName" ref="nickName" placeholder="닉네임 입력" />
-                <div v-if="errorList.nickNameBlank.length !== 0" class="error-text">{{ errorList.nickNameBlank }}</div>
+                <div v-if="registerErrorList.nickNameBlank.length !== 0" class="error-text">{{ registerErrorList.nickNameBlank }}</div>
+                <div v-if="errorList.alreadyNickName.length !== 0" class="error-text">{{ errorList.alreadyNickName }}</div>
+
                 <div class="register-input-container">
                     <input class="register-input" type="text" v-model="userEmail" ref="userEmail" placeholder="이메일" />
-                    <div v-if="errorList.emailBlank.length !== 0" class="login-email-error error-text">{{ errorList.emailBlank }}</div>
+                    <div v-if="registerErrorList.emailBlank.length !== 0" class="login-email-error error-text">
+                        {{ registerErrorList.emailBlank }}
+                    </div>
+                    <div v-if="errorList.alreadyEmail.length !== 0" class="error-text">{{ errorList.alreadyEmail }}</div>
                     <input class="passwd-input" type="password" v-model="userPassword" ref="password" placeholder="비밀번호" />
-                    <div v-if="errorList.passwordBlank.length !== 0" class="error-text">{{ errorList.passwordBlank }}</div>
+                    <div v-if="registerErrorList.passwordBlank.length !== 0" class="error-text">{{ registerErrorList.passwordBlank }}</div>
                 </div>
                 <div class="register-address-container">
                     <div class="register-adderss-title">주소(필수)</div>
-                    <SearchInputComponent :isRegister="true" @getSearchData="execDaumPostcode" />
+                    <SearchInputComponent :isRegister="true" @getSearchData="execDaumPostcode" ref="searchInput" />
                     <div>{{ addressList.city }} {{ addressList.district }} {{ addressList.street }}</div>
-                    <div v-if="errorList.addressBlank.length !== 0" class="address-error error-text">{{ errorList.addressBlank }}</div>
+                    <div v-if="registerErrorList.addressBlank.length !== 0" class="address-error error-text">
+                        {{ registerErrorList.addressBlank }}
+                    </div>
                 </div>
             </div>
+
             <div class="register__button" :class="{ 'register__button--active': addressList.active }" @click="clickSignUp">확인</div>
         </div>
     </div>
@@ -41,8 +49,13 @@ export default {
             userEmail: "",
             userPassword: "",
             addressList: { city: "", district: "", street: "", active: false },
-            errorList: { nickNameBlank: "", emailBlank: "", passwordBlank: "", addressBlank: "" },
+            registerErrorList: { nickNameBlank: "", emailBlank: "", passwordBlank: "", addressBlank: "" },
         };
+    },
+    computed: {
+        ...userHelper.mapState({
+            errorList: (state) => state.errorList,
+        }),
     },
     methods: {
         ...userHelper.mapActions(["postSignUp"]),
@@ -57,15 +70,17 @@ export default {
                 },
             }).open(this.$refs.embed);
         },
+
         resetErrorList() {
-            this.errorList.nickNameBlank = "";
-            this.errorList.emailBlank = "";
-            this.errorList.passwordBlank = "";
-            this.errorList.addressBlank = "";
+            this.registerErrorList.nickNameBlank = "";
+            this.registerErrorList.emailBlank = "";
+            this.registerErrorList.passwordBlank = "";
+            this.registerErrorList.addressBlank = "";
         },
+
         validateNickName() {
             if (this.userNickName.length === 0) {
-                this.errorList.nickNameBlank = "닉네임을 입력해주세요.";
+                this.registerErrorList.nickNameBlank = "닉네임을 입력해주세요.";
                 this.$refs.nickName.focus();
                 return true;
             }
@@ -74,11 +89,11 @@ export default {
         validateUserEmail() {
             const re = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
             if (this.userEmail.length === 0) {
-                this.errorList.emailBlank = "이메일을 입력해주세요.";
+                this.registerErrorList.emailBlank = "이메일을 입력해주세요.";
                 this.$refs.userEmail.focus();
                 return true;
             } else if (!re.test(this.userEmail)) {
-                this.errorList.emailBlank = "이메일 형식을 맞춰주세요.";
+                this.registerErrorList.emailBlank = "이메일 형식을 맞춰주세요.";
                 this.$refs.userEmail.focus();
                 return true;
             } else return false;
@@ -86,14 +101,16 @@ export default {
 
         validatePassword() {
             if (this.userPassword.length === 0) {
-                this.errorList.passwordBlank = "비밀번호를 입력해주세요.";
+                this.registerErrorList.passwordBlank = "비밀번호를 입력해주세요.";
                 this.$refs.password.focus();
                 return true;
             } else return false;
         },
+
         validateAddress() {
             if (this.addressList["city"].length === 0) {
-                this.errorList.addressBlank = "주소를 선택해주세요.";
+                this.registerErrorList.addressBlank = "주소를 선택해주세요.";
+                this.$refs.searchInput.focusInput();
                 return true;
             } else return false;
         },
@@ -111,6 +128,7 @@ export default {
             }
             return false;
         },
+
         clickSignUp() {
             if (this.validate()) return;
             this.postSignUp({
