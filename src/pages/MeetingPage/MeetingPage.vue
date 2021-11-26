2cf18da56ae03efe411eff077433fa3c8ef13678
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-26 18:19:38 +0900

    [update] 모임 페이지 수정

--- src/pages/MeetingPage/MeetingPage.vue
+++ src/pages/MeetingPage/MeetingPage.vue
@@ -6,19 +6,15 @@
             <FilterModalComponent ref="filterModal" />
             <div class="meeting-container">
                 <div class="meeting-status-bar">
-                    <div class="meeting-description">3900개의 모임이 기다리고 있어요</div>
+                    <div class="meeting-description">{{ postCount }}개의 모임이 기다리고 있어요</div>
                     <img class="meeting-filter-icon" @click="clickRateModal()" src="../../assets/images/MeetingPage/filter.png" />
                 </div>
-                <div>
-                    <MeetingChatComponent />
-                    <MeetingChatComponent />
-                    <MeetingChatComponent />
-                    <MeetingChatComponent />
-                    <MeetingChatComponent />
+                <div v-for="postItem in postList" :key="postItem.id">
+                    <MeetingChatComponent :postItem="postItem" />
                 </div>
             </div>
             <div>
-                <router-link to="/write">
+                <router-link to="/meeting/write">
                     <button class="meeting-write__button">
                         <img class="meeting-write__button-image" src="../../assets/images/PlusButtons/plus-black.png" />
                     </button>
@@ -31,6 +27,9 @@
 import Header from "../../layout/Header/Header.vue";
 import MeetingChatComponent from "../../components/MeetingChatComponent/MeetingChatComponent.vue";
 import FilterModalComponent from "../../components/FilterModalComponent/FilterModalComponent.vue";
+import { createNamespacedHelpers } from "vuex";
+const userHelper = createNamespacedHelpers("userStore");
+const postHelper = createNamespacedHelpers("postStore");
 
 export default {
     name: "MeetingPage",
@@ -42,7 +41,19 @@ export default {
     data() {
         return {};
     },
+
+    created() {
+        this.getMeetingListForLocation({ districtA: this.$route.query.districtA, districtB: this.$route.query.districtB });
+    },
+    computed: {
+        ...postHelper.mapState({
+            postList: (state) => state.postList,
+            postCount: (state) => state.postCount,
+        }),
+    },
     methods: {
+        ...userHelper.mapActions(["getMeetingListForLocation"]),
+
         clickRateModal() {
             this.$refs.filterModal.toggleVisibility();
             document.body.style.setProperty("overflow", "hidden");
